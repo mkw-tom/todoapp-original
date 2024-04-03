72cddd949f3625dced437b64404af7b8cd58b9f1
Tomu Maekawa <tomu.m1107@icloud.com>
2024-04-04 00:47:48 +0900

    addFormのフォームボタンを押してもデータが保存されるよう修正した。

--- src/app/components/AddForm.tsx
+++ src/app/components/AddForm.tsx
@@ -10,7 +10,7 @@ const AddForm = ({uid, todos, setTodos}: propsTask) => {
   const [text, setText] = useState<string>("");
   const ref = useRef<HTMLInputElement>(null!)
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleAdd = async (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     const newTodo: Task = {
       id: `${todos.length}`,
@@ -28,9 +28,9 @@ const AddForm = ({uid, todos, setTodos}: propsTask) => {
   }
 
   return (
-    <form className='w-full my-4' onSubmit={(e) => handleSubmit(e)}>
+    <form className='w-full my-4' onSubmit={(e) => handleAdd(e)}>
       <input type="text" className='rounded-l-md w-10/12' onChange={(e) => setText(e.target.value)} ref={ref}/>
-      <button className='bg-purple-800 text-white mb-2 rounded-r-md px-2'>+</button>
+      <button className='bg-purple-800 text-white mb-2 rounded-r-md px-2' onClick={(e) => handleAdd(e)}>+</button>
       <hr/>
     </form>
   )
